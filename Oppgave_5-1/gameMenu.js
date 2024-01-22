360c6ed6eb7dc2854ea6e5b269b57d9849af8a51
Arne-Thomas Aas Søndeled <45017875+atsond12@users.noreply.github.com>
2024-01-22 09:40:24 +0100

    Endring i libSprite.js

--- Oppgave_5-1/gameMenu.js
+++ Oppgave_5-1/gameMenu.js
@@ -27,7 +27,20 @@ export function TGameMenu(aCanvas, aSheetImage, aSheetData){
   pos.x -= 90;
   //pos.y += 110;
   const spGameOver = new TSprite(aCanvas, aSheetImage, aSheetData.gameOver, pos);
-  const spMedal =
+  
+  pos.x += 26;
+  pos.y += 43;
+  const spMedal = new TSprite(aCanvas, aSheetImage, aSheetData.medal, pos);
+  
+  pos.x += 0;
+  pos.y -= 0;
+  const spScore = new TSpriteNumber(aCanvas, aSheetImage, aSheetData.numberSmall, pos);
+
+  pos.x += 0;
+  pos.y += 50;
+  const spHeighScore = new TSpriteNumber(aCanvas, aSheetImage, aSheetData.numberSmall, pos);
+
+
   this.drawIdle = function(){
     spFlappyBird.draw();
     spButton.draw();
@@ -41,10 +54,16 @@ export function TGameMenu(aCanvas, aSheetImage, aSheetData){
   this.drawGameOver = function(){
     spGetReady.draw();
     spGameOver.draw();
+    spMedal.draw();
+    spScore.draw();
+    spHeighScore.draw();
   }
 
   this.setGameOver = function(){
     spGetReady.setIndex(1);
+    spMedal.setIndex(2);
+    spScore.setValue(100);
+    spHeighScore.setValue(200); 
   }
 
   function doCountDown(){
